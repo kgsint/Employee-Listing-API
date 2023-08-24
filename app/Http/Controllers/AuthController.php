562e7d395a79f0db59f0f576f7d08b60ee50ba17
Kaung Sint <kaungsintkoko99@gmail.com>
2023-08-24 10:31:53 +0630

    initialize login and register route | created requests for repective route and validate

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\LoginRequest;
+use App\Http\Requests\RegisterRequest;
+
+class AuthController extends Controller
+{
+    // Register
+    public function register(RegisterRequest $request)
+    {
+
+    }
+
+    // Login
+    public function login(LoginRequest $request)
+    {
+
+    }
+}
